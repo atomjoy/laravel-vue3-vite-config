982c38ddfbdd786b682236640a513b10eee8ad2e
atomjoy <atomjoy.official@gmail.com>
2023-03-30 13:33:42 +0200

    Update vite.config.js

--- vue/vite.config.js
+++ vue/vite.config.js
@@ -1,30 +1,35 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({    
-    plugins: [
-        laravel({
-            input: ['resources/js/app.js'],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-    // Change base url
-    // base: './', // Default url path is /build directory
-    // Or change assets dir
-	build: {
-		rollupOptions: {
-			output: {
-				assetFileNames: 'assets/[ext]/[name][extname]',
+export default defineConfig({
+	plugins: [
+		laravel({
+			input: ['resources/js/app.js'],
+			refresh: true,
+		}),
+		vue({
+			template: {
+				transformAssetUrls: {
+					base: null,
+					includeAbsolute: false,
+				},
 			},
-		},
+		}),
+	],
+	// Change base default url /build directory
+	base: './',
+	build: {
+		emptyOutDir: true,
 	},
-});
+	// Or change assets dir
+	// build: {
+	// 	rollupOptions: {
+	// 		output: {
+	// 			assetFileNames: 'assets/[ext]/[name].[hash].[extname]',
+	// 			chunkFileNames: 'chunks/[name].[hash].js',
+	// 			entryFileNames: 'js/[name].[hash].js',
+	// 		},
+	// 	},
+	// },
+})
