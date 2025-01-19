95c01680edc0525465d11b939820f0f36a20e395
Atomjoy <atomjoy.official@gmail.com>
2025-01-19 17:41:50 +0100

    Update app.js

--- vue/resources/js/app.js
+++ vue/resources/js/app.js
@@ -16,24 +16,25 @@ app.use(i18n)
 app.use(stores)
 app.use(router)
 
-app.provide('globalStore', {
-	id: 1,
-	async getUsers() {
-		let res = await axios.get('https://jsonplaceholder.typicode.com/users')
-		// console.log('App global store', res.data)
-	},
-})
+// // Global data 
+// app.provide('globalStore', {
+// 	id: 1,
+// 	async getUsers() {
+// 		let res = await axios.get('https://jsonplaceholder.typicode.com/users')
+// 		// console.log('App global store', res.data)
+// 	},
+// })
 
-// v-color="'red'"
-app.directive('color', (el, binding) => {
-	el.style.color = binding.value
-})
+// // v-color="'red'"
+// app.directive('color', (el, binding) => {
+// 	el.style.color = binding.value
+// })
 
-// v-highlight="'yellow'"
-app.directive('highlight', {
-	mounted(el, binding, vnode) {
-		el.style.background = binding.value
-	},
-})
+// // v-highlight="'yellow'"
+// app.directive('highlight', {
+// 	mounted(el, binding, vnode) {
+// 		el.style.background = binding.value
+// 	},
+// })
 
 app.mount('#app')
