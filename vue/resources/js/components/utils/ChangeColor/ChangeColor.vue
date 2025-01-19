d9919fd641b534cca55681c16eeef0c222b7b2f4
Atomjoy <atomjoy.official@gmail.com>
2025-01-19 18:35:14 +0100

    Vue components

---
+++ vue/resources/js/components/utils/ChangeColor/ChangeColor.vue
@@ -0,0 +1,23 @@
+<script setup>
+let props = defineProps({
+	color: { type: String, default: '#0099ff' },
+	text: { type: String, default: 'Colored Text' },
+	size: { type: String, default: '25px' },
+})
+</script>
+
+<template>
+	<span class="colored-text">{{ text }}</span>
+</template>
+
+<style scoped>
+.wrapp{
+	--color: v-bind(props.color);
+}
+
+.colored-text {
+	margin-right: 20px;
+	color: v-bind('props.color');
+	font-size: v-bind('props.size');
+}
+</style>
